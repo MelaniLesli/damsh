0e416a9159e5b14241414cab3c1a95761b7248c5
Mel <m3l4n1f4n@hotmail.com>
2022-01-26 10:54:56 +0100

    estructura de proyecto aun en proceso

--- damsh/prompt/llamada_prompt.py
+++ damsh/prompt/llamada_prompt.py
@@ -1,7 +1,8 @@
 import subprocess, shlex, sys
 
-from Damsh as eje_linea
+import Damsh as eje_linea
 import iteractivo as a
 import lectura_fichero as b
 
 """Ejecutar el main"""
+
---
+++ principal.py
@@ -0,0 +1,6 @@
+#ejecuto help
+import shlex
+x = 10
+
+def menu():
+    
