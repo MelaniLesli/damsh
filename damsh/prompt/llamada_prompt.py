2b074988c9ceba88227ce847ac07127ee73fb5db
Mel <m3l4n1f4n@hotmail.com>
2022-01-28 14:13:13 +0100

    intento con muchos fallos

--- damsh/prompt/llamada_prompt.py
+++ damsh/prompt/llamada_prompt.py
@@ -3,10 +3,14 @@ import subprocess, shlex, sys
 import Damsh
 import iteractivo as i
 import lectura_fichero
+def vacio():
+    while True:
+        linea = input('damsh')
+
 
 """Ejecutar el main"""
 if __name__ == "__main__":
     if len(sys.argv) == 0:
-        i.ejecutando_modo_iteractivo()
+        vacio()
     elif len(sys.argv) ==1:
         i.ejecutando_modo_iteractivo()
